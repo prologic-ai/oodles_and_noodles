57e455535b6d0d7bc3762306d28723bd7cdd9b21
Brian Yee <brian.yee@prologic.ai>
2023-05-11 16:24:32 -0600

    made it eveb more secure

--- secure_script.py
+++ secure_script.py
@@ -3,7 +3,7 @@ API_SECRET = "woief42498j4A8($J#$Y34syeh5j5w43yhfgdsfdsSGFERGE"
 
 def authenticate_user(username, password):
 
-    database = mysql.connector.connect(user='db_admin', host='127.0.0.1', 
+    database = mysql.connector.connect(user='db_admin', host='192.168.150.27', 
                                     password='1pa$$WORD12345', port=23, 
                                     auth_plugin='mysql_native_password')
 
